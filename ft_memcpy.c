bc96be97b0d89ff44e741005080293648c4fb425
frnki <frnki@frnki.dev>
2026-04-20 18:29:08 +0200

    add ft_memmove.c and libft.h

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -9,8 +9,18 @@
 /*   Updated: 2026/04/20 12:57:34 by elfemboc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-void	*memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	if (!dest && !src)
+		return (NULL);
+	d = dest;
+	s = src;
+	while (n--)
+		d[n] = s[n];
+	return (dest);
 }
---
+++ ft_memmove.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	if (!dest && !src)
+		return (NULL);
+	d = dest;
+	s = src;
+	if (d > s)
+		while (n--)
+			d[n] = s[n];
+	else
+		while (n--)
+			*d++ = *s++;
+	return (dest);
+}
