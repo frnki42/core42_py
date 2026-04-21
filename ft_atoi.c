0d594bfd2de5ad165ae2718c57f65538c04e47d7
frnki <frnki@frnki.dev>
2026-04-21 16:33:31 +0200

    add ft_atoi.c and ft_strnstr.c

---
+++ ft_atoi.c
@@ -0,0 +1,26 @@
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (nptr[i] == 32 || (nptr[i] > 8 && nptr[i] < 14))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+		if (nptr[i++] == '-')
+			sign = -sign;
+	i--;
+	while (nptr[++i] >= '0' && nptr[i] <= '9')
+		result = result * 10 + nptr[i] - '0';
+	return (result * sign);
+}
+/*
+#include <stdio.h>
+int	main()
+{
+	printf("%i\n", ft_atoi("420"));
+}
+*/
