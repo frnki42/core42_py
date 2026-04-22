8256b94b8afcaa2428942c2595e097fcdc12e1b7
frnki <frnki@frnki.dev>
2026-04-22 22:33:03 +0200

    add ft_lstadd_front.c

---
+++ libft/ft_lstadd_front.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
+/*
+#include <stdio.h>
+int	main()
+{
+	t_list	*tmp;
+	t_list	*nde1 = ft_lstnew("into a linked list?");
+	t_list	*nde2 = ft_lstnew("does this turn");
+
+	ft_lstadd_front(&nde1, nde2);
+	while (nde1)
+	{
+		printf("%s\n", (char *)nde1->content);
+		tmp = nde1;
+		nde1 = nde1->next;
+		free(tmp);
+	}
+}
+*/
--- libft/libft.h
+++ libft/libft.h
@@ -21,5 +21,6 @@ char	*ft_strdup(const char *s);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+t_list	*ft_lstnew(void *content);
 
 #endif
