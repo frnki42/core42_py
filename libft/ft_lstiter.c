856ba94858a52134e6ab148cfaf44a947d082487
frnki <frnki@frnki.dev>
2026-04-23 01:12:37 +0200

    add ft_lstiter.c

---
+++ libft/ft_lstiter.c
@@ -0,0 +1,36 @@
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+
+	if (!lst || !f)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
+/*
+#include <stdio.h>
+void	print_content(void *content)
+{
+	printf("%s\n", (char *)content);
+}
+
+int	main()
+{
+	t_list	*tmp;
+	t_list	*lst = ft_lstnew("1");
+
+	ft_lstadd_back(&lst, ft_lstnew("2"));
+	ft_lstadd_back(&lst, ft_lstnew("3"));
+	ft_lstiter(lst, print_content);
+	while (lst)
+	{
+		tmp = lst;
+		lst = lst->next;
+		free(tmp);
+	}
+}
+*/
