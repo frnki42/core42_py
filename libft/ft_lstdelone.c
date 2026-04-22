cf3c79e206a30091388f89297ee42d5b16101f4d
frnki <frnki@frnki.dev>
2026-04-23 00:21:25 +0200

    add ft_lstdelone.c

---
+++ libft/ft_lstdelone.c
@@ -0,0 +1,34 @@
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return ;
+	del(lst->content);
+	free(lst);
+}
+/*
+#include <stdio.h>
+void	del(void *content)
+{
+	free(content);
+}
+
+int	main()
+{
+	char	*str;
+	t_list	*nde;
+
+	str = malloc(7);
+	if (!str)
+		return (printf("malloc failed\n"), 1);
+	ft_memcpy(str, "test42", 7);
+	nde = ft_lstnew(str);
+	if (!nde)
+		return (printf("ft_lstnew() failed\n"), 1);
+	printf("nde->content before ft_lstdelone() = %s\n", (char *)nde->content);
+	ft_lstdelone(nde, del);
+	printf("ft_lstdelone completed\n");
+	return (0);
+}
+*/
