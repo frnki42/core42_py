8f0175f459086c623e7773194fedaa6c26f3b6fc
frnki <frnki@frnki.dev>
2026-04-22 17:25:17 +0200

    add ft_itoa.c

---
+++ libft/ft_itoa.c
@@ -0,0 +1,58 @@
+#include "libft.h"
+
+static size_t	get_size(long nbr)
+{
+	size_t	size;
+
+	size = 1;
+	if (nbr < 0)
+		size++;
+	while (nbr /= 10)
+		size++;
+	return (size);
+}
+
+static char	*set_str(long nbr, size_t size)
+{
+	char	*str;
+	size_t	start;
+
+	str = malloc(size + 1);
+	if (!str)
+		return (NULL);
+	start = 0;
+	if (nbr < 0)
+	{
+		nbr = -nbr;
+		str[0] = '-';
+		start = 1;
+	}
+	str[size] = '\0';
+	while (size-- > start)
+	{
+		str[size] = nbr % 10 + '0';
+		nbr /= 10;
+	}
+	return (str);
+}
+
+char	*ft_itoa(int n)
+{
+	long	nbr;
+	size_t	size;
+
+	nbr = n;
+	size = get_size(nbr);
+	return (set_str(nbr, size));
+}
+/*
+#include <stdio.h>
+int	main()
+{
+	char	*free_me;
+
+	free_me = ft_itoa(0);
+	printf("%s\n", free_me);
+	free(free_me);
+}
+*/
--- libft/libft.h
+++ libft/libft.h
@@ -5,11 +5,11 @@
 # include <stdint.h>
 
 size_t	ft_strlen(const char *str);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-int	ft_isalpha(char c);
-int	ft_isdigit(char c);
+int		ft_isalpha(char c);
+int		ft_isdigit(char c);
 char	*ft_strdup(const char *s);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 char	*ft_strchr(const char *s, int c);
