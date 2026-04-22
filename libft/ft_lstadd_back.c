3954b782d8926907c361019d4c99bf1641c1bb16
frnki <frnki@frnki.dev>
2026-04-22 23:50:09 +0200

    add ft_lstadd_back.c

---
+++ libft/ft_lstadd_back.c
@@ -0,0 +1,31 @@
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*tmp;
+
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	tmp = *lst;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+}
+/*
+#include <stdio.h>
+int	main()
+{
+	t_list	*nde1 = ft_lstnew("nde1");
+	t_list	*nde2 = ft_lstnew("nde2");
+
+	ft_lstadd_back(&nde1, nde2);
+	printf("%i\n", ft_lstsize(nde1));
+	free(nde1);
+	free(nde2);
+}
+*/
--- libft/libft.h
+++ libft/libft.h
@@ -23,5 +23,6 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int	ft_lstsize(t_list *lst);
 
 #endif
