488fd838183c8aed7f4327ed329ff021def0e1cb
frnki <frnki@frnki.dev>
2026-04-23 02:02:55 +0200

    add ft_lstmap.c

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,66 @@
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*nde;
+	t_list	*new;
+	void	*cnt;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	new = NULL;
+	while (lst)
+	{
+		cnt = f(lst->content);
+		nde = ft_lstnew(cnt);
+		if (!nde)
+		{
+			del(cnt);
+			ft_lstclear(&new, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new, nde);
+		lst = lst->next;
+	}
+	return (new);
+}
+/*
+#include <stdio.h>
+void	del(void *content)
+{
+	free(content);
+}
+
+void	*make_zero(void *content)
+{
+	(void)content;
+	return (ft_strdup("0"));
+}
+
+int	main()
+{
+
+	t_list	*tmp;
+	t_list	*lst;
+	t_list	*new;
+
+	lst = ft_lstnew("1");
+	ft_lstadd_back(&lst, ft_lstnew("2"));
+	ft_lstadd_back(&lst, ft_lstnew("3"));
+	new = ft_lstmap(lst, make_zero, del);
+	while (new)
+	{
+		printf("%s\n", (char *)new->content);
+		tmp = new;
+		new = new->next;
+		del(tmp->content);
+		free(tmp);
+	}
+	while (lst)
+	{
+		tmp = lst;
+		lst = lst->next;
+		free(tmp);
+	}
+}
+*/
