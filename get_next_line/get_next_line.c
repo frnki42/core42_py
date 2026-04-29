f4066ef392475de9f6089062e79ec1be9bbd590b
frnki <frnki@frnki.dev>
2026-04-29 15:24:49 +0200

    add main function to get_next_line.c for testing

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,1 +1,32 @@
-test
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: .frnki <frnki@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/20 04:20:42 by .frnki            #+#    #+#             */
+/*   Updated: 2026/04/20 16:20:42 by .frnki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	
+}
+
+#include <fcntl.h>
+int	main(int argc, char **argv)
+{
+	int	fd;
+
+	if (argc < 2)
+		return (write(1, "NAH BRO\n", 8));
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		return (write(1, "invalid file\n", 13), 1);
+	get_next_line(fd);
+	close(fd);
+	return (0);
+}
