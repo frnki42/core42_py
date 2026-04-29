08a88bd0622d7f7dc9d7c2f10caa4edd6e470952
frnki <frnki@frnki.dev>
2026-04-29 18:43:41 +0200

    add changes to get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -11,26 +11,61 @@
 /* ************************************************************************** */
 #include "get_next_line.h"
 #include <stdio.h>					//remove me
-char	*get_next_line(int fd)
-{
-	char	buf[BUFFER_SIZE];
+#include <fcntl.h>					//remove me
 
-	if (read(fd, buf, BUFFER_SIZE) == -1)
-		return (NULL);
-	printf("%s", buf);
+int	no_newline(char **buf)
+{
+	printf("no_newline got executed\n");
+	return (1);
 }
 
-#include <fcntl.h>
+char	*set_buf(char **buf, int fd)
+{
+	ssize_t	size;
+
+	*buf = malloc(BUFFER_SIZE + 1);
+	if (!*buf)
+		return (NULL);
+	size = 42;
+	while (size > 0 && no_newline(buf))
+	{
+		size = read(fd, *buf, BUFFER_SIZE);
+		if (size == -1)
+			return (free(*buf), *buf = NULL, NULL);
+		printf("%s\n", *buf);
+		if (size == 0)
+			printf("size = 0");
+	}
+	(*buf)[size] = '\0';
+	return (*buf);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buf;
+	char		*gnl;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	buf = set_buf(&buf, fd);
+	if (!buf)
+		return (NULL);
+	return (buf);
+}
+
 int	main(int argc, char **argv)
 {
-	int	fd;
+	int		fd;
+	char	*gnl;
 
 	if (argc < 2)
 		return (write(1, "NAH BRO\n", 8));
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		return (write(1, "invalid file\n", 13), 1);
-	get_next_line(fd);
+	gnl = get_next_line(fd);
+	printf("%s\n",gnl);
+	free(gnl);
 	close(fd);
 	return (0);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -14,9 +14,9 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1024
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 
 char	*get_next_line(int fd);
 
