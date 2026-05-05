bf1f63393cb8846f4a20a15ad501deeac62b883f
frnki <frnki@frnki.dev>
2026-05-05 14:11:34 +0200

    add correct buffer management

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -28,7 +28,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char		*d;
 	const unsigned char	*s;
 
-	if (!dest && !src)
+	if (!dest || !src)
 		return (NULL);
 	d = dest;
 	s = src;
@@ -41,21 +41,25 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
-char	*gnl_strjoin(char const *s1, char const *s2)
+char	*gnl_strjoin(char *s1, char *s2)
 {
 	char	*str;
 	size_t	len_s1;
 	size_t	len_s2;
 
-	if (!s1 || !s2)
+	if (!s2)
 		return (NULL);
-	len_s1 = ft_strlen(s1);
+	len_s1 = 0;
+	if (s1)
+		len_s1 = ft_strlen(s1);
 	len_s2 = ft_strlen(s2);
 	str = malloc(len_s1 + len_s2 + 1);
 	if (!str)
 		return (NULL);
-	ft_memmove(str, s1, len_s1);
+	if (s1)
+		ft_memmove(str, s1, len_s1);
 	ft_memmove(&str[len_s1], s2, len_s2 + 1);
+	free(s1);
 	return (str);
 }
 
@@ -75,16 +79,16 @@ int	no_newline(char *buf)
 char	*set_buf(char *buf, int fd)
 {
 	ssize_t	size;
-	char	src[BUFFER_SIZE + 1];
+	char	dst[BUFFER_SIZE + 1];
 
 	size = 1;
-	while (no_newline(buf) && size > 0 )
+	while (no_newline(buf) && size)
 	{
-		size = read(fd, src, BUFFER_SIZE);
+		size = read(fd, dst, BUFFER_SIZE);
 		if (size == -1)
-			return (free(buf), buf = NULL, NULL);
-		src[size] = '\0';
-		buf = gnl_strjoin(buf, src);
+			return (free(buf), buf = NULL);
+		dst[size] = '\0';
+		buf = gnl_strjoin(buf, dst);
 		if (!buf)
 			return (NULL);
 	}
@@ -94,14 +98,14 @@ char	*set_buf(char *buf, int fd)
 char	*get_next_line(int fd)
 {
 	static char	*buf;
-	char		*gnl;
+//	char		*gnl;
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	buf = set_buf(buf, fd);
 	if (!buf)
 		return (NULL);
-	return (gnl);
+	return (buf);
 }
 
 int	main(int argc, char **argv)
@@ -114,8 +118,11 @@ int	main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		return (write(1, "invalid file\n", 13), 1);
+	gnl = NULL;
 	gnl = get_next_line(fd);
-	printf("%s\n",gnl);
+	if (!gnl)
+		return (free(gnl), 2);
+	printf("%s\n", gnl);
 	free(gnl);
 	close(fd);
 	return (0);
