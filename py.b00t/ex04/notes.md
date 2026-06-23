9036be630cca3b181b531e82623061c32a9e1d37
frnki <frnki@frnki.dev>
2026-06-23 23:04:49 +0200

    add solution for ex04

---
+++ py.b00t/ex04/notes.md
@@ -0,0 +1,1 @@
+how are we supposed to handle only 1 argument? 
--- py.b00t/ex04/operations.py
+++ py.b00t/ex04/operations.py
@@ -1,13 +1,30 @@
 import sys
 
-def main():
+
+def operations():
+    if len(sys.argv) < 3:
+        print("Usage: python operations.py <number1> <number2>")
+        print("Example:")
+        print("    python operations.py 10 3")
+        return
+    assert len(sys.argv) < 4, "too many arguments"
     try:
-        A = int(sys.argv[1])
-        B = int(sys.argv[2])
+        a = int(sys.argv[1])
+        b = int(sys.argv[2])
     except ValueError:
-        raise AssertionError("only integers")
-    print(f"Sum:        {A + B}")
-    print(f"Difference: {A - B}")
-    print(f"Product:    {A * B}")
-    print(f"Quotient:   {A / B}")
-    print(f"Remainder:  {A % B}")
+        raise AssertionError("only integers") from None
+    print(f"Sum:        {a + b}")
+    print(f"Difference: {a - b}")
+    print(f"Product:    {a * b}")
+    try:
+        print(f"Quotient:   {a / b}")
+    except ZeroDivisionError:
+        print("Quotient:   ERROR (division by zero)")
+    try:
+        print(f"Remainder:  {a % b}")
+    except ZeroDivisionError:
+        print("Remainder:  ERROR (modulo by zero)")
+
+
+if __name__ == "__main__":
+    operations()
