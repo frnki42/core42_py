3e1a8a2fdc4ba68d6580f3e6e7a8ee8cbc4a1937
frnki <frnki@frnki.dev>
2026-09-08 13:59:28 +0200

    modify code to make mypy --strict happy

--- py.00/ex3/ft_harvest_total.py
+++ py.00/ex3/ft_harvest_total.py
@@ -1,4 +1,4 @@
-def ft_harvest_total():
+def ft_harvest_total() -> None:
     harvest = int(input("Day 1 harvest: "))
     harvest += int(input("Day 2 harvest: "))
     harvest += int(input("Day 3 harvest: "))
--- py.00/ex6/ft_count_harvest_recursive.py
+++ py.00/ex6/ft_count_harvest_recursive.py
@@ -1,10 +1,10 @@
-def count_harvest(start, days):
+def count_harvest(start: int, days: int) -> None:
     if start <= days:
         print(f"Day {start}")
         count_harvest(start + 1, days)
 
 
-def ft_count_harvest_recursive():
+def ft_count_harvest_recursive() -> None:
     days = int(input("Days until harvest: "))
     count_harvest(1, days)
     print("Harvest time!")
