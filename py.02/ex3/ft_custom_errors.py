7493308e70f15fb70a98060c9513c6c5bea84833
frnki <frnki@frnki.dev>
2026-09-09 23:27:02 +0200

    ex3? wip

---
+++ py.02/ex3/ft_custom_errors.py
@@ -0,0 +1,18 @@
+class GardenError(Exception):
+    ...
+
+
+class   PlantError(Exception):
+    ...
+
+
+class   WaterError(Exception):
+    ...
+
+
+
+def main() -> None:
+
+
+if __name__ == "__main__":
+    main()
