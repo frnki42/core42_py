06226c8624292a071178287de833b513ce58c504
frnki <frnki@frnki.dev>
2026-09-13 02:20:29 +0200

    add test main to py.03/ex3

--- py.03/ex3/ft_achievement_tracker.py
+++ py.03/ex3/ft_achievement_tracker.py
@@ -1,8 +1,17 @@
-import random
+# import random
 
 
 def main() -> None:
     print("=== Achievement Tracker System ===\n")
+    alice = {"OG", "World Savior", "Master Explorer", "Unstoppable"}
+    bob = {"OG", "Strategist", "World Savior", "Master Explorer"}
+    print(alice)
+    print(bob)
+    print(alice & bob)
+    print(alice | bob)
+    print(alice ^ bob)
+    print(alice - bob)
+    print(bob - alice)
 
 
 if __name__ == "__main__":
