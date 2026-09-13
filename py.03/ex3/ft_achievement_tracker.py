18d06b9d62013776cf5b03f587bce3c53b5663c4
frnki <frnki@frnki.dev>
2026-09-13 03:53:56 +0200

    add current state of py.03/ex3 - wip

--- py.03/ex3/ft_achievement_tracker.py
+++ py.03/ex3/ft_achievement_tracker.py
@@ -1,17 +1,39 @@
-# import random
+import random
+
+
+ACHIEVEMENTS = [
+        "Crafting Genius",
+        "Strategist",
+        "World Savior",
+        "Speed Runner",
+        "Survivor",
+        "Master Explorer",
+        "Treasure Hunter",
+        "Unstoppable",
+        "First Steps",
+        "Collector Supreme",
+        "Untouchable",
+        "Sharp Mind",
+        "Boss Slayer",
+        ]
+
+
+def gen_player_achievements() -> None:
+    amount_avs = len(ACHIEVEMENTS)
+    alice = set(random.sample(ACHIEVEMENTS, random.randint(0, amount_avs)))
+    bob = set(random.sample(ACHIEVEMENTS, random.randint(0, amount_avs)))
+    charlie = set(random.sample(ACHIEVEMENTS, random.randint(0, amount_avs)))
+    dylan = set(random.sample(ACHIEVEMENTS, random.randint(0, amount_avs)))
+    print(f"Player Alice: {alice}")
+    print(f"Player Bob: {bob}")
+    print(f"Player Charlie: {charlie}")
+    print(f"Player Dylan: {dylan}")
+    print(f"\nAll distinct achievements: {ACHIEVEMENTS}\n")
 
 
 def main() -> None:
     print("=== Achievement Tracker System ===\n")
-    alice = {"OG", "World Savior", "Master Explorer", "Unstoppable"}
-    bob = {"OG", "Strategist", "World Savior", "Master Explorer"}
-    print(alice)
-    print(bob)
-    print(alice & bob)
-    print(alice | bob)
-    print(alice ^ bob)
-    print(alice - bob)
-    print(bob - alice)
+    gen_player_achievements()
 
 
 if __name__ == "__main__":
