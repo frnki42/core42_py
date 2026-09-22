660d8339bc0f5d63ea38321ead840ca365c98ed8
frnki <frnki@frnki.dev>
2026-09-22 19:36:08 +0200

    py.05/ex0: implement DataProcessor base and NumericProcessor

--- py.05/ex0/data_processor.py
+++ py.05/ex0/data_processor.py
@@ -2,21 +2,45 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 
+def is_number(x: Any) -> bool:
+    return isinstance(x, int | float) and not isinstance(x, bool)
+
+
+def all_numbers(xs: Any) -> bool:
+    if not isinstance(xs, list):
+        return False
+    return all(is_number(x) for x in xs)
+
+
 class DataProcessor(ABC):
+    def __init__(self) -> None:
+        self._items: list[tuple[int, str]] = []
+        self._count: int = 0
+
     @abstractmethod
     def validate(self, data: Any) -> bool:
-        return True
+        ...
 
     @abstractmethod
     def ingest(self, data: Any) -> None:
         ...
 
     def output(self) -> tuple[int, str]:
-        ...
+        return self._items.pop(0)
 
 
 class NumericProcessor(DataProcessor):
-    ...
+    def validate(self, data: Any) -> bool:
+        return is_number(data) or all_numbers(data)
+
+    def ingest(self, data: int | float | list[int | float]) -> None:
+        if not self.validate(data):
+            raise TypeError("Improper numeric data")
+        if not isinstance(data, list):
+            data = [data]
+        for item in data:
+            self._items.append((self._count, str(item)))
+            self._count += 1
 
 
 class TextProcessor(DataProcessor):
@@ -28,7 +52,10 @@ class LogProcessor(DataProcessor):
 
 
 def main() -> None:
-    print("=== Code Nexus - Data Processor ===")
+    print("=== Code Nexus - Data Processor ===\n")
+    print("Testing Numeric Processor...")
+    numeric = NumericProcessor()
+    print(numeric.validate(42))
 
 
 if __name__ == "__main__":
